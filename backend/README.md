b4aa1279adf12121dd13d5ed4b8e2d680bbf155a
Eric Burlingame <burlingamesoftware@gmail.com>
2021-05-01 07:23:03 -0700

    Initial commit with basic WS backend

---
+++ backend/README.md
@@ -0,0 +1,46 @@
+# Disruptors backend
+## Install pipenv for manage venvs and packages
+
+You can find pipenv guide here: https://realpython.com/pipenv-guide/
+
+Some advanced pipenv techniques you can find here: https://pipenv-fork.readthedocs.io/en/latest/advanced.html
+```bash
+pip3 install pipenv
+```
+#### To activate the virtual environment type
+```bash
+pipenv shell
+```
+#### Install default packages
+```bash
+pipenv install
+```
+#### Install dev packages
+```bash
+pipenv install --dev
+```
+#### add local .env file and update it to align to your local env if necessary
+```bash
+cp .env.example .env
+```
+#### Build project from docker
+```bash
+make build
+```
+
+#### run project
+```bash
+make up
+```
+
+#### refresh requirements.txt from pipfile.lock
+```bash
+pipenv lock -r > requirements.txt
+```
+
+#### for testing
+just run `pytest`
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ backend/app/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, Depends, Request, WebSocket
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, FileResponse
+from aioredis import create_redis_pool, Redis
+
+from app import config
+
+global_settings = config.Settings()
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+
+async def init_redis_pool() -> Redis:
+    redis = await create_redis_pool(
+        global_settings.redis_url,
+        password=global_settings.redis_password,
+        encoding="utf-8",
+        db=global_settings.redis_db,
+    )
+    return redis
+
+
+@app.on_event("startup")
+async def starup_event():
+    app.state.redis = await init_redis_pool()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    app.state.redis.close()
+    await app.state.redis.wait_closed()
+
+
+@app.get("/")
+async def index():
+    return FileResponse("static/index.html")
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Hello from server: {data}")
+
+
+@app.get("/health-check")
+async def health_check(settings: config.Settings = Depends(config.get_settings)):
+    try:
+        await app.state.redis.set(str(settings.redis_url), settings.up)
+        value = await app.state.redis.get(str(settings.redis_url))
+    except:  # noqa: E722
+        value = settings.down
+    return {settings.web_server: settings.up, str(settings.redis_url): value}
