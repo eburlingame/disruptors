3a64d6e0425e7ca299541e93e27b8ef7c874d9e9
Eric Burlingame <burlingamesoftware@gmail.com>
2021-06-16 09:38:39 -0700

    Adding charts for dice roll distribution

--- frontend/src/components/GameSummary.tsx
+++ frontend/src/components/GameSummary.tsx
@@ -1,10 +1,10 @@
-import { Box, VStack } from "@chakra-ui/layout";
+import { Box, HStack, VStack } from "@chakra-ui/layout";
 import React from "react";
 import { useSessionState } from "../hooks/session";
 import { responsiveVisibiity } from "../responsiveUtils";
 import { GameState } from "../state/atoms";
+import { CatanGameSummary } from "../state/game_types";
 import { getRoomPlayer } from "../utils/utils";
-import Bank from "./Bank";
 import GameBoard from "./GameBoard";
 import GameColumn from "./GameColumn";
 import { GameViewState, GameStateContext } from "./GameView";
@@ -12,33 +12,84 @@ import Layout from "./Layout";
 import Players from "./Players";
 import TabContainer from "./TabContainer";
 import TabSet from "./TabSet";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
+import { Button } from "@chakra-ui/button";
+
+const DiceChart = ({ diceFrequencies }: { diceFrequencies: number[] }) => {
+  const data = diceFrequencies.map((value, index) => ({
+    name: index.toString(),
+    dice: value,
+  }));
+
+  return (
+    <BarChart width={730} height={250} data={data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" label="Frequency" color="#aaa" min={1} max={12} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="dice" fill="#8884d8" />
+    </BarChart>
+  );
+};
 
 export default ({ gameState }: { gameState: GameState }) => {
   const { room } = useSessionState();
+  const gameViewState: GameViewState = { gameState };
 
-  const gameViewState: GameViewState = {
-    gameState,
-  };
+  if (!room || !room.gameSummary) {
+    return <Layout>Invalid game summary</Layout>;
+  }
+
+  const gameSummary = room.gameSummary as CatanGameSummary;
 
   const players = gameState.state.players.map((player) => ({
     ...getRoomPlayer(room, player.playerId),
     ...player,
   }));
 
-  const winner = players.find((player) => player.points >= 10);
+  const winner = players.find(
+    (player) => player.playerId === gameSummary.winner
+  );
 
   return (
     <GameStateContext.Provider value={gameViewState}>
       <Layout>
-        <Box textAlign="center" fontWeight="bold" fontSize="xx-large">
-          {winner?.name} won!
-        </Box>
+        <HStack justifyContent="space-between" marginY="2" p="2">
+          <Box>
+            <Button colorScheme="green">Back to lobby</Button>
+          </Box>
+          <Box textAlign="center" fontWeight="bold" fontSize="xx-large">
+            {winner?.name} won!
+          </Box>
+          <Box></Box>
+        </HStack>
 
         <Box flex="1" display="flex">
           {/* Left columns */}
           <GameColumn flex="1" hiddenOn="sm">
             <TabContainer flex="1">
-              <TabSet tabs={[{ name: "Players", content: Players }]} />
+              <TabSet
+                tabs={[
+                  { name: "Players", content: Players },
+                  {
+                    name: "Dice Rolls",
+                    content: () => (
+                      <DiceChart
+                        diceFrequencies={gameSummary.diceFrequencies}
+                      />
+                    ),
+                  },
+                ]}
+              />
             </TabContainer>
           </GameColumn>
 
